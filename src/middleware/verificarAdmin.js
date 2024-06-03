78118c531d77907fdefcebaefc973fb264fb699a
NicolasPL <31411531+NicolasPL64@users.noreply.github.com>
2024-06-03 09:38:08 -0500

    Update verificarAdmin.js

--- src/middleware/verificarAdmin.js
+++ src/middleware/verificarAdmin.js
@@ -1,7 +1,7 @@
 const ErrorStatus = require('../utilidades/ErrorStatus');
 
 function verificarAdmin(req, res, next) {
-    if (req.params.tabla != "reserva") {
+    if (req.params.tabla != "reserva" && req.params.tabla != "cliente") {
         if (req.usuario.modoAdmin) next();
         else throw new ErrorStatus('Sólo los administradores pueden realizar esta acción.', 401);
     }
