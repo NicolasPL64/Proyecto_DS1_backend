885c3abb63ed4f34524fc9d1555976f813d4987b
NicolasPL <31411531+NicolasPL64@users.noreply.github.com>
2024-08-24 13:00:59 -0500

    Create verificarToken.js

---
+++ src/middleware/verificarToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const ErrorStatus = require('../utilidades/ErrorStatus');
+
+function verificarToken(req, res, next) {
+    const { token } = req.cookies;
+    if (!token) {
+        throw new ErrorStatus('No tienes permiso para realizar esta acción.', 401);
+    }
+    try {
+        const datos = jwt.verify(token, process.env.JWT_SECRET);
+        req.usuario = datos;
+        next();
+    } catch (error) {
+        throw new ErrorStatus('Token inválido.', 401);
+    }
+}
+
+module.exports = verificarToken;
